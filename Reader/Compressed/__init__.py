cbca8de9773ebe4d187b9a19e79dcc479ac39579
Sarvex Jatasra <sarvex.jatasra@gmail.com>
2015-04-03 11:03:01 +0530

    Packages

--- Reader/Compressed/__init__.py
+++ Reader/Compressed/__init__.py
@@ -1,1 +1,6 @@
 __author__ = 'Sarvex'
+
+from Reader.Compressed.bzipped import opener as bz2_opener
+from Reader.Compressed.gzipped import opener as gzip_opener
+
+__all__ = ['bz2_opener', 'gzip_opener']
